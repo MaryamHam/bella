3b7d9d4cea80d686b7c9f2187912e867986d158a
MaryamHam <107949861+MaryamHam@users.noreply.github.com>
2023-08-31 13:40:14 +0400

    Adam, Bella and Carl collect tokens in a game.

---
+++ bin/bella.dart
@@ -0,0 +1,26 @@
+
+//Adam, Bella and Carl collect tokens in a game. 
+//Bella has 10 more tokens than Adam. 
+//Carl has 8 more tokens than Bella.
+// Altogether they have 91 tokens. 
+// How many tokens does Adam have?
+void main() {
+
+int bellaTokens;
+int adamTokens;
+int carlTokens;
+
+adamTokens = 0;
+
+bellaTokens = 10 + adamTokens;
+
+carlTokens = 18 + adamTokens;
+
+adamTokens = 91 - bellaTokens-carlTokens;
+
+
+adamTokens = (63/ 3).toInt();
+print("Adam has $adamTokens tokens. ");
+
+
+}
---
+++ test/bella_test.dart
@@ -0,0 +1,8 @@
+import 'package:bella/bella.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
